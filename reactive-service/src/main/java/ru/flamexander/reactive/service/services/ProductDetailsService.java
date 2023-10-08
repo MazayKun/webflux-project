e2b3cd7354323ac1282dbb7119ec534c719b5d5a
MazayKun <kir99916@gmail.com>
2023-10-08 15:01:37 +0300

    Добавил задание по WebFlux

--- reactive-service/src/main/java/ru/flamexander/reactive/service/services/ProductDetailsService.java
+++ reactive-service/src/main/java/ru/flamexander/reactive/service/services/ProductDetailsService.java
@@ -2,16 +2,28 @@ package ru.flamexander.reactive.service.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.flamexander.reactive.service.dtos.ProductDetailsDto;
 import ru.flamexander.reactive.service.integrations.ProductDetailsServiceIntegration;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductDetailsService {
+
     private final ProductDetailsServiceIntegration productDetailsServiceIntegration;
 
     public Mono<ProductDetailsDto> getProductDetailsById(Long id) {
         return productDetailsServiceIntegration.getProductDetailsById(id);
+    }
+
+    public Flux<ProductDetailsDto> getProductDetailsByIds(List<Long> ids) {
+        return productDetailsServiceIntegration.getProductDetailsByIds(ids);
+    }
+
+    public Flux<ProductDetailsDto> getAllProductsDetails() {
+        return productDetailsServiceIntegration.getAllProductsDetails();
     }
 }
--- slow-service/src/main/java/ru/flamexander/product/details/service/controllers/ProductDetailsController.java
+++ slow-service/src/main/java/ru/flamexander/product/details/service/controllers/ProductDetailsController.java
@@ -1,20 +1,51 @@
 package ru.flamexander.product.details.service.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.flamexander.product.details.service.dtos.ProductDetailsDto;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.isNull;
+
 @RestController
 @RequestMapping("/api/v1/details")
 public class ProductDetailsController {
-    @GetMapping("/{id}")
-    public ProductDetailsDto getProductDetailsById(@PathVariable Long id) throws InterruptedException {
-        if (id > 100) {
-            throw new RuntimeException();
-        }
+
+    private final Map<Long, ProductDetailsDto> dataMap = Map.of(
+            1L, new ProductDetailsDto(1L, "First description"),
+            2L, new ProductDetailsDto(2L, "Second description")
+    );
+    @GetMapping("/")
+    public Collection<ProductDetailsDto> getAllProductsDetails() throws InterruptedException {
         Thread.sleep(2500 + (int)(Math.random() * 2500));
-        return new ProductDetailsDto(id, "Product description..");
+        return dataMap.values();
+    }
+
+    @GetMapping("/{ids}")
+    public ResponseEntity<?> getProductDetailsByIds(@PathVariable List<Long> ids) throws InterruptedException {
+        Thread.sleep(2500 + (int)(Math.random() * 2500));
+        if(ids.size() == 1) {
+            return getProductDetailsById(ids.get(0));
+        }
+        List<ProductDetailsDto> result = new ArrayList<>();
+        for (Long id : ids) {
+            result.add(dataMap.get(id));
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    private ResponseEntity<ProductDetailsDto> getProductDetailsById(@PathVariable Long id) throws InterruptedException {
+        ProductDetailsDto result = dataMap.get(id);
+        if(isNull(result)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
     }
 }
